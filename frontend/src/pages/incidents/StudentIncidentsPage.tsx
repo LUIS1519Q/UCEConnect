90631edc0c39bd06356f57211606a85f493adc90
Nayeli Guayas <guayasnayeli2003@gmail.com>
2026-07-04 21:22:44 -0500

    fix: update place for dashboard layout

--- frontend/src/pages/incidents/StudentIncidentsPage.tsx
+++ frontend/src/pages/incidents/StudentIncidentsPage.tsx
@@ -1,4 +1,4 @@
-import DashboardLayout from "../../components/DashboardLayout";
+import DashboardLayout from "../../components/ui/templates/DashboardLayout";
 import { Link } from "react-router-dom";
 
 function StudentIncidentsPage() {
