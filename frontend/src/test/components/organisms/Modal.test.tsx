290b0f867d345abe1c9fd2f78de0affa30d6a550
Nayeli Guayas <guayasnayeli2003@gmail.com>
2026-07-05 23:14:01 -0500

    refactor(ui): move modal to organisms

--- frontend/src/test/components/organisms/Modal.test.tsx
+++ frontend/src/test/components/organisms/Modal.test.tsx
@@ -1,7 +1,7 @@
 import { fireEvent, render, screen } from "@testing-library/react";
 import { describe, expect, it, vi } from "vitest";
 
-import { Modal } from "../../../components/ui/molecules";
+import { Modal } from "../../../components/ui/organisms";
 
 describe("Modal", () => {
   it("renders title and content when open", () => {
